f47aa5261c0e21d8d740fdb61ba89fda0d03dbb7
yugandhar-d <yugandhar-d@users.noreply.github.com>
2020-05-15 17:37:41 +0530

    Code file created.

---
+++ data-science-project-1/code.py
@@ -0,0 +1,74 @@
+# --------------
+# Code starts here
+
+
+# Create the lists 
+class_1=['Geffrey Hinton','Andrew Ng','Sebastian Raschka','yoshua bengio']
+class_2=['Hilary Mason','Carla Gentry','Corinna Cortes']
+# Concatenate both the strings
+new_class=class_1 + class_2
+print(new_class)
+
+# Append the list
+new_class.append('Peter Warden')
+# Print updated list
+print(new_class)
+
+# Remove the element from the list
+new_class.remove('Carla Gentry')
+# Print the list
+
+print(new_class)
+
+
+# Create the Dictionary
+courses={'Math':65,'English':70,'History':80,'French':70,'Science':60}
+total=sum(courses.values()) 
+print(total)
+percentage=(total*100)/500
+print(percentage)
+# Slice the dict and stores  the all subjects marks in variable
+
+mathematics={'Geffrey Hinton':78,'Andrew Ng':95,'Sebastian Raschka':65,'yoshua bengio':50,'Hilary Mason':70,'Corinna Cortes':66,'Peter Warden':75}
+# Store the all the subject in one variable `Total`
+Total=max(mathematics.values())
+# Print the total
+print(Total)
+val_list=list(mathematics.values())
+key_list=list(mathematics.keys())
+topper=(key_list[val_list.index(Total)]) 
+print(topper)
+#ff=topper.split(' ')
+first_name,last_name=topper.split(' ')
+#print(ff)
+full_name=last_name+' '+first_name
+certificate_name=full_name.upper()
+print(certificate_name)
+# Insert percentage formula
+
+# Print the percentage
+
+
+
+
+# Create the Dictionary
+ 
+
+
+
+# Given string
+
+
+# Create variable first_name 
+
+# Create variable Last_name and store last two element in the list
+
+# Concatenate the string
+
+# print the full_name
+
+# print the name in upper case
+
+# Code ends here
+
+
